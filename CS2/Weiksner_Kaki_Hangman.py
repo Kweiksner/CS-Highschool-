caccc1e0bf8b876414aaecd08d217b3d4adc8b2d
Kweiksner <kweiksner27@gcds.net>
2024-12-09 19:03:54 -0500

    Add files via upload

--- CS2/Weiksner_Kaki_Hangman.py
+++ CS2/Weiksner_Kaki_Hangman.py
@@ -98,40 +98,64 @@ def diagram(lives):
               /   \ |
               =======          
                 ''')
+    else:
+        print()
     
     
-def check_guess(word):
-  new_list = []
-  for i in len(word): 
-      new_list.append("_")
-  print(new_list)
-  #for index in len(word):
-     # if word(index) == guess: 
-         # print("hi")
+def check_guess(guessed_correctly,word, guess):
+    new_list = []
+    for i in range(len(word)): 
+        letter = word[i]
+        if letter == guess:
+            new_list.append(guess)
+            guessed_correctly.append(guess)
+        else:
+            if letter in guessed_correctly:
+                new_list.append(letter)
+            else:
+                new_list.append("_")
+    return new_list
+ 
             
                       
 
 def main():
-    lives = 7 
-    word = ["kaki"]
-    print("Welcome to Hangman")
-    print(diagram(lives))
-    while lives > 0: #loop for everything
-        word = random.choice(word)
-        go = 0 
-        while go == 0: #loop to make sure that user entered a letter
-            guess = input("Guess a letter: ")
-            if guess.isalpha():
-                    go = 1
-            else:
-              print("You did not enter a letter")
-        if guess not in word: 
-            lives = lives-1
-            print("letter is not in word")
-        else: 
-            check_guess(word)
+    play_again = "yes"
+    while play_again == "yes": 
+        guessed_correctly = []
+        lives = 7 
+        word = ["kaki"]
+        print("Welcome to Hangman")
         print(diagram(lives))
+        word = random.choice(word)
+
+        while lives > 0: #loop for everything
+            go = 0 
+            while go == 0: #loop to make sure that user entered a letter
+                guess = input("Guess a letter: ")
+                if guess.isalpha():
+                        go = 1
+                else:
+                    lives -= 1
+                    print("You did not enter a letter")
+            if guess not in word: 
+                print("letter is not in word")
+            else: 
+                letters_guessed = check_guess(guessed_correctly,word,guess)
+                print(letters_guessed)
+            print(diagram(lives))
+            print(f"You have {lives} left")
+        play_again = str.lower(input("Would you like to play agin"))
+        while True:
+            if play_again == "no": 
+                print("Have a good day")
+            elif play_again == "yes":
+                continue
+            else: 
+                print("Enter Yes or no")
+                break
+
+
             
 main()
-
 
